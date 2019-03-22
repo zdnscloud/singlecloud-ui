7784904abc5eee0c6406d8754a04934383acc70f
gsmlg <me@gsmlg.org>
2019-03-22 16:31:34 +0800

    fix for test

--- packages/ui/app/containers/LanguageProvider/tests/reducer.test.js
+++ packages/ui/app/containers/LanguageProvider/tests/reducer.test.js
@@ -7,7 +7,7 @@ describe('languageProviderReducer', () => {
   it('returns the initial state', () => {
     expect(languageProviderReducer(undefined, {})).toEqual(
       fromJS({
-        locale: 'en',
+        locale: 'zh',
       })
     );
   });
