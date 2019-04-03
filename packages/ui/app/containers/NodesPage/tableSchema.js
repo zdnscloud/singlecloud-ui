9f6be657a788e034e85dcea3f766ebae194f84e6
gsmlg <me@gsmlg.org>
2019-04-03 17:01:17 +0800

    optimize label

--- packages/ui/app/containers/NodesPage/tableSchema.js
+++ packages/ui/app/containers/NodesPage/tableSchema.js
@@ -1,4 +1,6 @@
+import React, { Fragment } from 'react';
 import { ucfirst } from '@gsmlg/utils';
+import Chip from '@material-ui/core/Chip';
 
 const schema = [
   'name',
@@ -17,6 +19,19 @@ const schema = [
 const tableSchema = schema.map((id) => ({
   id,
   label: ucfirst(id),
-}));
+}))
+  .map((item) => {
+    if (item.id === 'labels') {
+      return {
+        ...item,
+        component(props) {
+          return props.data
+            .get('labels')
+            .map((val, key) => <Chip label={`${key}=${val}`} />).toList();
+        },
+      };
+    }
+    return item;
+  });
 
 export default tableSchema;
