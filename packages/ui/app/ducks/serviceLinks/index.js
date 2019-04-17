5d6b6786a650126d8bd745c381bcfca970bd85ab
gsmlg <me@gsmlg.org>
2019-04-17 15:33:56 +0800

    map names

--- packages/ui/app/ducks/serviceLinks/index.js
+++ packages/ui/app/ducks/serviceLinks/index.js
@@ -3,6 +3,7 @@
  * Service Links Duck
  *
  */
+import _ from 'lodash';
 import { fromJS } from 'immutable';
 import getByKey from '@gsmlg/utils/getByKey';
 import { procCollectionData } from '@gsmlg/utils/procData';
@@ -31,7 +32,20 @@ export const serviceLinksReducer = (
     case c.LOAD_OUTER_SERVICES_SUCCESS: {
       const { clusterID, namespaceID } = meta;
       const { data } = payload.response;
-      return state.updateIn(['outerServices', clusterID, namespaceID], () => fromJS(data));
+      const resData = data && data.map((d) => {
+        const name = d.port === 0 ? `http$${d.domain}` : `udp$${d.port}`;
+        const services = {};
+        _.each(d.services, (s, p) => {
+          services[p] = s;
+        });
+
+        return {
+          ...d,
+          name,
+          services,
+        };
+      });
+      return state.setIn(['outerServices', clusterID, namespaceID], fromJS(data));
     }
     case c.LOAD_OUTER_SERVICES_FAILURE:
       return state;
@@ -41,7 +55,19 @@ export const serviceLinksReducer = (
     case c.LOAD_INNER_SERVICES_SUCCESS: {
       const { clusterID, namespaceID } = meta;
       const { data } = payload.response;
-      return state.updateIn(['innerServices', clusterID, namespaceID], () => fromJS(data));
+      const resData = data && data.map((d) => ({
+        ...d,
+        name: `svc$${d.name}`,
+        workloads: d.workloads && d.workloads.map((w) => ({
+          ...w,
+          name: `deploy$${w.name}`,
+          pods: w.pods && w.pods.map((p) => ({
+            ...p,
+            name: `pod$${p.name}`,
+          })),
+        })),
+      }));
+      return state.setIn(['innerServices', clusterID, namespaceID], fromJS(resData));
     }
     case c.LOAD_INNER_SERVICES_FAILURE:
       return state;
