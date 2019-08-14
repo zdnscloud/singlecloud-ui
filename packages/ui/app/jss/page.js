a7363f99e08dff077c52920b9e5eb3dd8a21fb45
gsmlg <me@gsmlg.org>
2019-08-14 20:05:18 +0800

    updating plop

---
+++ packages/ui/app/jss/page.js
@@ -0,0 +1,114 @@
+const styles = (theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    fontSize: '14px',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  select: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    overflow: 'auto',
+  },
+  content: {
+    flexGrow: 1,
+    overflow: 'auto',
+    overflowX: 'hidden',
+  },
+  grid: {
+    padding: theme.spacing(2),
+  },
+  appContainer: {},
+  advanceContainer: {},
+  actionContainer: {
+    textAlign: 'right',
+  },
+  h5: {
+    marginBottom: theme.spacing(2),
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  textField: {
+    flexBasis: 200,
+  },
+  padding10: {
+    padding: '10px',
+  },
+  marginRight10: {
+    marginRight: '10px',
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    flexBasis: 200,
+    minWidth: 194,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  addContainerWrap: {
+    minHeight: '75px',
+  },
+  addContainerButton: {
+    alignSelf: 'center',
+  },
+  cardCategoryWhite: {
+    '&,& a,& a:hover,& a:focus': {
+      color: 'rgba(255,255,255,.62)',
+      margin: '0',
+      fontSize: '14px',
+      marginTop: '0',
+      marginBottom: '0',
+    },
+    '& a,& a:hover,& a:focus': {
+      color: '#FFFFFF',
+    },
+  },
+  cardTitleWhite: {
+    color: '#FFFFFF',
+    marginTop: '0px',
+    minHeight: 'auto',
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: '3px',
+    textDecoration: 'none',
+    '& small': {
+      color: '#777',
+      fontSize: '65%',
+      fontWeight: '400',
+      lineHeight: '1',
+    },
+  },
+  separateLineWrap: {
+    marginTop: '8px',
+    marginRight: '5px',
+    background: '#eee',
+  },
+  separateLine: {
+    marginTop: '8px',
+    boxShadow: '1px 0px 2px #ccc',
+    border: '1px solid #ccc',
+  },
+  removeBtn: {
+    float: 'right',
+  },
+  createBtnLink: {
+    position: 'absolute',
+    top: '3px',
+    right: '10px',
+  },
+  radioControl: {
+    flexDirection: 'row',
+  },
+  radioLabel: {
+    flexDirection: 'row',
+    lineHeight: 3,
+  },
+  radioGroup: {
+    flexDirection: 'row',
+    marginLeft: '2rem',
+  },
+});
+
+export default styles;
