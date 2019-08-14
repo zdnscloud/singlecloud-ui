02364051ab2b1e3ce4558dd7cce5dcd885f6a3c9
gsmlg <me@gsmlg.org>
2019-08-14 10:32:23 +0800

    fix bug

--- packages/ui/app/components/Helmet/Helmet.js
+++ packages/ui/app/components/Helmet/Helmet.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-
 import { useIntl } from 'react-intl';
-
-import messages from './messages';
 
 const SingleCloudHelmet = ({ title, description }) => {
   const intl = useIntl();
