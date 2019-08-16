499c6cfa71ce5f4d0fbda21b52f8ed6d73e74df9
gsmlg <me@gsmlg.org>
2019-08-16 19:40:50 +0800

    fix

--- packages/ui/app/containers/ServicesPage/CreatePage.js
+++ packages/ui/app/containers/ServicesPage/CreatePage.js
@@ -25,6 +25,7 @@ import GridItem from 'components/Grid/GridItem';
 import GridContainer from 'components/Grid/GridContainer';
 import Breadcrumbs from 'components/Breadcrumbs/Breadcrumbs';
 
+import { makeSelectLocation } from 'ducks/app/selectors';
 import { makeSelectCurrentID as makeSelectClusterID } from 'ducks/clusters/selectors';
 import { makeSelectCurrentID as makeSelectNamespaceID } from 'ducks/namespaces/selectors';
 import { makeSelectURL } from 'ducks/services/selectors';
@@ -57,15 +58,40 @@ export const CreateServicePage = ({
   deployList,
   dsList,
   stsList,
+  location,
 }) => {
   const classes = useStyles();
+  const search = location.get('search');
+  let from = false;
+  let targetResourceType = '';
+  let targetName='';
+  if (search && search.includes('from=true')) {
+    const [trt, type] = /targetResourceType=([a-zA-Z]+)/i.exec(search);
+    const [tn, name] = /targetName=([a-zA-Z0-9-]+)/i.exec(search);
+    from = true;
+    targetResourceType = type;
+    targetName = name;
+  }
   useEffect(() => {
     loadDeployments(deployURL, { clusterID, namespaceID });
     loadDaemonSets(dsURL, { clusterID, namespaceID });
     loadStatefulSets(stsURL, { clusterID, namespaceID });
   }, [deployURL, dsURL, stsURL]);
+  let exposedPorts = [];
+  if (from) {
+    let l = [];
+    if (targetResourceType === 'deployments') l = deployList;
+    if (targetResourceType === 'daemonSets') l = dsList;
+    if (targetResourceType === 'statefulSets') l = stsList;
+    const item = l.find((i) => i.get('name') === targetName);
+    if (item) {
+      exposedPorts = item.get('exposedPorts');
+    }
+  }
   const initialValues =  fromJS({
-    targetResourceType: 'deployments',
+    targetResourceType: from ? targetResourceType : 'deployments',
+    targetName: from ? targetName : '',
+    name: from ? targetName : '',
     serviceType: 'clusterip',
     exposedPorts: [],
   });
@@ -146,6 +172,7 @@ const mapStateToProps = createStructuredSelector({
   deployList: makeSelectDeploymentsList(),
   dsList: makeSelectDaemonSetsList(),
   stsList: makeSelectStatefulSetsList(),
+  location: makeSelectLocation(),
 });
 
 const mapDispatchToProps = (dispatch) =>
