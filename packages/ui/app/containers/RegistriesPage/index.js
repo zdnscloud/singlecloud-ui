7bf9df0b4efce6f86538cd83fbb713c64d7a5b2d
zhanqiongqiong <461791369@qq.com>
2019-08-20 11:58:33 +0800

    add registriespage

---
+++ packages/ui/app/containers/RegistriesPage/index.js
@@ -0,0 +1,76 @@
+/**
+ *
+ * Registries
+ *
+ */
+import React, { useEffect, useState, memo } from 'react';
+import PropTypes from 'prop-types';
+import { createStructuredSelector } from 'reselect';
+import { bindActionCreators, compose } from 'redux';
+import { connect } from 'react-redux';
+
+import Helmet from 'components/Helmet/Helmet';
+import { FormattedMessage } from 'react-intl';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+// import { makeSelectCurrentID as makeSelectClusterID } from 'ducks/clusters/selectors';
+// import { makeSelectCurrentID as makeSelectNamespaceID } from 'ducks/namespaces/selectors';
+import { makeSelectURL } from 'ducks/registries/selectors';
+import * as actions from 'ducks/registries/actions';
+
+import useStyles from './styles';
+import messages from './messages';
+import RegistriesTable from './Table';
+
+const Registries = ({
+  // clusterID,
+  // namespaceID,
+  location,
+  url,
+  loadRegistries,
+}) => {
+  const classes = useStyles();
+  useEffect(() => {
+    if (url) {
+      loadRegistries(url, {
+        // clusterID,
+        // namespaceID,
+      });
+    }
+    return () => {
+      // try cancel something when unmount
+    };
+  }, [url]);
+
+  return (
+    <div className={classes.root}>
+      <Helmet title={messages.pageTitle} description={messages.pageDesc} />
+      <CssBaseline />
+      <div className={classes.content}></div>
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  // clusterID: makeSelectClusterID(),
+  // namespaceID: makeSelectNamespaceID(),
+  url: makeSelectURL(),
+});
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(
+    {
+      ...actions,
+    },
+    dispatch
+  );
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps
+);
+
+export default compose(
+  withConnect,
+  memo
+)(Registries);
