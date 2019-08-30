4d69f977a9c458f0bef5eb0eec061af0bc9ae77b
gsmlg <me@gsmlg.org>
2019-08-30 14:23:59 +0800

    remove useless deps

--- packages/com/src/SimpleTable.js
+++ packages/com/src/SimpleTable.js
@@ -6,7 +6,6 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
