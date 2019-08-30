ac70c6dbf320b73826583dbd694e08a7f5ee52b3
gsmlg <me@gsmlg.org>
2019-08-30 11:36:53 +0800

    update SimpleTable

--- packages/com/src/SimpleTable.js
+++ packages/com/src/SimpleTable.js
@@ -1,6 +1,6 @@
 /**
  *
- * ClustersPage
+ * SimpleTable
  *
  */
 
@@ -14,66 +14,69 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
-export class SimpleTable extends React.PureComponent {
-  static propTypes = {
-    schema: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.object,
-      ]).isRequired,
-    })).isRequired,
-    data: PropTypes.object.isRequired,
-  };
-
-  getKey(data, key) {
+export const SimpleTable = ({
+  schema,
+  data,
+  ...extras
+}) => {
+  const getKey = (data, key) => {
     if (data && data.get && typeof data.get === 'function') {
       return data.get(key);
     } else if (typeof data === 'object') {
       return data[key];
     }
     return null;
-  }
+  };
 
-  render() {
-    const { schema, data, ...extras } = this.props;
-
-    return (
-      <Table {...extras}>
-        <TableHead>
-          <TableRow>
-            {schema.map((column) => (
-              <TableCell key={this.getKey(column, 'id')} component="th">
-                {this.getKey(column, 'label')}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.filter((d) => !!d).map((rowData, id) => (
-            <TableRow key={id}>
-              {schema.map((column) => {
-                const CustomComponent = this.getKey(column, 'component');
-                return (
-                  <TableCell key={this.getKey(column, 'id')}>
-                    {(CustomComponent != null) ? (
-                      <CustomComponent
-                        {...(column.props || {})}
-                        data={rowData}
-                        column={column}
-                        id={this.getKey(column, 'id')}
-                        value={this.getKey(rowData, this.getKey(column, 'id'))}
-                      />
-                    ) : `${this.getKey(rowData, this.getKey(column, 'id'))}`}
-                  </TableCell>
-                );
-              })}
-            </TableRow>
+  return (
+    <Table {...extras}>
+      <TableHead>
+        <TableRow>
+          {schema.map((column) => (
+            <TableCell key={getKey(column, 'id')} component="th">
+              {getKey(column, 'label')}
+            </TableCell>
           ))}
-        </TableBody>
-      </Table>
-    );
-  }
-}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data.filter((d) => !!d).map((rowData, id) => (
+          <TableRow key={id}>
+            {schema.map((column) => {
+              const CustomComponent = getKey(column, 'component');
+              return (
+                <TableCell key={getKey(column, 'id')}>
+                  {(CustomComponent != null) ? (
+                    <CustomComponent
+                      {...(column.props || {})}
+                      data={rowData}
+                      column={column}
+                      id={getKey(column, 'id')}
+                      value={getKey(rowData, getKey(column, 'id'))}
+                    />
+                  ) : `${getKey(rowData, getKey(column, 'id'))}`}
+                </TableCell>
+              );
+            })}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+SimpleTable.propTypes = {
+  schema: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object,
+    ]).isRequired,
+  })).isRequired,
+  data: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array,
+  ]).isRequired,
+};
 
 export default SimpleTable;
