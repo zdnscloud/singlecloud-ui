520ad72abd815a80626403bd93129d5c1b4d92ad
zhanqiongqiong <461791369@qq.com>
2019-09-29 10:04:36 +0800

    remove card boxShadow

--- packages/ui/app/components/Card/cardStyle.js
+++ packages/ui/app/components/Card/cardStyle.js
@@ -9,7 +9,7 @@ const cardStyle = (theme) => ({
     color: `rgba(${hexToRgb(theme.palette.text.a)}, 0.87)`,
     background: theme.palette.light.main,
     width: '100%',
-    boxShadow: `0 1px 4px 0 rgba(${hexToRgb(theme.palette.text.a)}, 0.14)`,
+    // boxShadow: `0 1px 4px 0 rgba(${hexToRgb(theme.palette.text.a)}, 0.14)`,
     position: 'relative',
     display: 'flex',
     flexDirection: 'column',
