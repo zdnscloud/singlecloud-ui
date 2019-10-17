ca19653b406483067aac134c90acf6a30499f1d9
gsmlg <me@gsmlg.org>
2019-10-17 18:12:02 +0800

    fix platform

--- packages/benchmark/karma.conf.js
+++ packages/benchmark/karma.conf.js
@@ -57,10 +57,9 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
-        'Chrome',
-        'IE',
-        'Firefox',
-        'Safari',
+      'Chrome',
+      'Firefox',
+      process.platform === 'darwin' ? 'Safari' : 'IE',
     ],
 
 
