22e745e1b49236acf87def70e612d65c3fda6b55
zhanqiongqiong <461791369@qq.com>
2019-11-11 10:33:50 +0800

    nodeList add status

--- packages/ui/app/containers/NodesPage/messages.js
+++ packages/ui/app/containers/NodesPage/messages.js
@@ -45,6 +45,10 @@ export default defineMessages({
     id: `${scope}.tableTitleLabels`,
     defaultMessage: 'Labels',
   },
+  tableTitleStatus: {
+    id: `${scope}.tableTitleStatus`,
+    defaultMessage: 'Status',
+  },
   tableTitleCreationTimestamp: {
     id: `${scope}.tableTitleCreationTimestamp`,
     defaultMessage: 'CreationTimestamp',
