53b86df1317bdc3a40be47efa666358fa7ad0b70
gsmlg <me@gsmlg.org>
2019-11-20 11:19:38 +0800

    fix

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -51,24 +51,8 @@ server {
   }
 
   location @passlinkerd {
-    # Allow websocket connections
-    proxy_set_header Upgrade $http_upgrade;
-    proxy_set_header Connection $connection_upgrade;
-    proxy_set_header X-Real-IP  $remote_addr;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header Host $http_host;
-    proxy_http_version 1.1;
-    chunked_transfer_encoding off;
-    proxy_buffering off;
-    proxy_cache_bypass $http_pragma;
-    proxy_cache_revalidate on;
-    if ($uri ~ ^/apis/zcloud.cn/v1/clusters/([^\/]+)/linkerd(.+)$) {
-      set $cluster $1;
-      set $linkerduri $2;
-    }
-    rewrite ^/apis/zcloud.cn/v1/clusters/[^\/]+/linkerd(.+)$ $1 break;
-    rewrite ^/clusters/[^\/]+/linkerd/grafana(.+)$ /grafana$1 break;
-    proxy_pass http://@linkerd;
+    set $saved_redirect_location '$upstream_http_location';
+    proxy_pass $saved_redirect_location;
   }
 
   location ~ ^/(apis|web) {
