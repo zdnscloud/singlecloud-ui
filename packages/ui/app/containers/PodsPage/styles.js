fd69b44fde02459de0c9ad173b98ff16c057d432
zhanqiongqiong <461791369@qq.com>
2019-11-24 11:56:50 +0800

    fix  pod dialogCardHeader style

--- packages/ui/app/containers/PodsPage/styles.js
+++ packages/ui/app/containers/PodsPage/styles.js
@@ -1,16 +1,10 @@
 import { makeStyles } from '@material-ui/styles';
 import pageStyles from 'jss/page';
+import dialogStyles from 'jss/dialog';
 
 export const styles = (theme) => ({
   ...pageStyles(theme),
-  dialogCard: {
-    margin: 0,
-  },
-  dialogHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
+  ...dialogStyles(theme),
   dialogCardBody: {
     height: 'calc(100vh - 225px)',
   },
