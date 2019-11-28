b7ef999adbe4dcceda0c94523cd95afb40de99a1
zhanqiongqiong <461791369@qq.com>
2019-11-28 18:14:28 +0800

    fix pod table

--- packages/ui/app/containers/PodsPage/tableSchema.js
+++ packages/ui/app/containers/PodsPage/tableSchema.js
@@ -6,6 +6,10 @@ import { FormattedMessage } from 'react-intl';
 import Confirm from 'components/Confirm/Confirm';
 import IconButton from 'components/CustomIconButtons/IconButton';
 import RestartIcon from 'components/Icons/Restart';
+// import IconButton from '@material-ui/core/IconButton';
+import ShellIcon from 'components/Icons/Shell';
+import LogIcon from 'components/Icons/Log';
+import Chip from '@material-ui/core/Chip';
 import messages from './messages';
 
 const schema = ['state', 'name', 'nodeName', 'containers', 'creationTimestamp'];
@@ -20,6 +24,71 @@ const tableSchema = schema
       return {
         ...item,
         component: TimeCell,
+      };
+    }
+    if (item.id === 'containers') {
+      return {
+        ...item,
+        component({
+          value,
+          clusterID,
+          namespaceID,
+          openTerminal,
+          classes,
+          openPodLog,
+          data,
+        }) {
+          return value != null
+            ? value.map((ctn, i) => (
+                <Chip
+                  key={i}
+                  label={
+                    <Fragment>
+                      {ctn.get('image')}
+                      <IconButton
+                        aria-label="View Log"
+                        className={classes.button}
+                        size="small"
+                        edge="end"
+                        style={{ transform: 'scale(0.7)' }}
+                        onClick={(evt) => {
+                          openPodLog(
+                            {
+                              podID: data.get('id'),
+                              containerName: ctn.get('name'),
+                            },
+                            {
+                              clusterID,
+                              namespaceID,
+                            }
+                          );
+                        }}
+                      >
+                        <LogIcon />
+                      </IconButton>
+                      <IconButton
+                        aria-label="Terminal"
+                        className={classes.button}
+                        size="small"
+                        edge="end"
+                        style={{ transform: 'scale(0.7)' }}
+                        onClick={(evt) => {
+                          openTerminal('pod', {
+                            clusterID,
+                            namespaceID,
+                            podID: data.get('id'),
+                            containerName: ctn.get('name'),
+                          });
+                        }}
+                      >
+                        <ShellIcon />
+                      </IconButton>
+                    </Fragment>
+                  }
+                />
+              ))
+            : null;
+        },
       };
     }
     return item;
