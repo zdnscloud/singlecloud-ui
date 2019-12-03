7a395f683a59e0a18a6f95963d628e9640af83a9
zhanqiongqiong <461791369@qq.com>
2019-12-03 14:38:36 +0800

    fixed width of  timecell

--- packages/ui/app/components/Cells/TimeCell.js
+++ packages/ui/app/components/Cells/TimeCell.js
@@ -6,7 +6,11 @@ const TimeCell = ({ value }) => {
   const time = dayjs(value);
   const format = 'YYYY-MM-DD HH:mm:ss';
 
-  return <time dateTime={value}>{time.format(format)}</time>;
+  return (
+    <time dateTime={value} style={{ minWidth: 140, display: 'inline-block' }}>
+      {time.format(format)}
+    </time>
+  );
 };
 
 export default TimeCell;
