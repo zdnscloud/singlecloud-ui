9e790b1e41b72ebcd29e68bb370bce86da51cdd0
gsmlg <me@gsmlg.org>
2019-12-11 15:18:30 +0800

    add stories in com

--- packages/com/src/NetworkGraph.js
+++ packages/com/src/NetworkGraph.js
@@ -9,6 +9,8 @@ import {
   WithTooltip, withTooltipPropTypes,
 } from '@data-ui/network';
 
+import DirectedLink from './NetworkGraph/DirectedLink';
+
 export {
   Network, networkPropTypes,
   withScreenSize, withParentSize, ParentSize,
@@ -18,3 +20,35 @@ export {
   Atlasforcedirectedlayout,
   WithTooltip, withTooltipPropTypes,
 }
+
+const NetworkGraph = ({
+  animated,
+  ariaLabel,
+  width,
+  height,
+  renderTooltip,
+  margin,
+  graph,
+  onClick,
+  renderNode,
+  ...rest
+}) => {
+
+  return (
+    <Network
+      {...rest}
+      renderTooltip={renderTooltip}
+      width={width}
+      height={height}
+      margin={margin}
+      animated={animated}
+      ariaLabel={ariaLabel}
+      graph={graph}
+      onClick={onClick}
+      renderNode={renderNode}
+      renderLink={DirectedLink}
+    />
+  );
+};
+
+export default NetworkGraph;
