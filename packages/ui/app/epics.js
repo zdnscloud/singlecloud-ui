40d6fc2c2d0a8e14ca51a30eb3db85d1505abbde
gsmlg <me@gsmlg.org>
2020-02-03 11:56:12 +0800

    fix missing comma

--- packages/ui/app/epics.js
+++ packages/ui/app/epics.js
@@ -98,7 +98,7 @@ export default function createEpic(injectedEpics = {}) {
     svcMeshPodsEpic,
     svcMeshTapEpic,
     alarmsEpic,
-    thresholdsEpic
+    thresholdsEpic,
     persistentVolumeClaimsEpic,
     persistentVolumesEpic
     // combine epics end
