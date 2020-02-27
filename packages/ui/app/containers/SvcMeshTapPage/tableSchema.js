6bc1b545a15a6287ad974771028258b001776b80
gsmlg <me@gsmlg.org>
2020-02-27 20:39:36 +0800

    fix link

--- packages/ui/app/containers/SvcMeshTapPage/tableSchema.js
+++ packages/ui/app/containers/SvcMeshTapPage/tableSchema.js
@@ -174,12 +174,12 @@ const tableSchema = schema
               <OpenInNewIcon fontSize="small" />
             </IconButton>
           );
-          const link = cur.get('link').replace('/apis/zcloud.cn/v1', '');
+          const curLink = (cur.get('link') || '').replace('/apis/zcloud.cn/v1', '');
 
           return (
             <Grid container direction="row" alignItems="center" spacing={1}>
               <Grid item>
-                <Button link component={Link} to={`${link}/show`}>
+                <Button link component={Link} to={`${curLink}/show`}>
                   {cur.get('deployment') || cur.get('statefulset') || cur.get('daemonset')}
                 </Button>
               </Grid>
